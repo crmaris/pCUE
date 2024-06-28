9a56f753ed0a9100a44967329ab61327574ee7c1
crmaris <crmaris@gmail.com>
2024-06-28 16:10:07 +0300

    v1.3

--- pCUE/MainWindow.xaml.cs
+++ pCUE/MainWindow.xaml.cs
@@ -774,14 +774,18 @@ namespace pCUE
             {
                 foreach (System.Diagnostics.Process pr in System.Diagnostics.Process.GetProcesses()) //GETS PROCESSES
                 {
-                    if ((pr.ProcessName == "CueLLAccessService") || (pr.ProcessName == "Corsair.Service.CpuIdRemote64")
+                    if ((pr.ProcessName == "CueLLAccessService") || (pr.ProcessName == "Corsair.Service.CpuIdRemote64") || (pr.ProcessName == "Corsair.Service.CpuIdRemote")
                         || (pr.ProcessName == "Corsair.Service.DisplayAdapter") || (pr.ProcessName == "Corsair.Service"))
                     {
                         pr.Kill(); //KILLS THE PROCESSES
+                        //ForceKill(pr);
                     }
                 }
             }
-            catch { }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
         }
     
 
@@ -790,9 +794,59 @@ namespace pCUE
             Process[] processes = Process.GetProcessesByName(App);
             foreach (var process in processes)
             {
-                process.Kill();
+                //process.Kill();
+                ForceKill(process);
                 //break;
             }
+        }
+
+        public static void ForceKill(Process proc)
+        {
+
+            // Accessing ProcessName could throw an exception if the process has already been killed.
+            string processName = string.Empty;
+            try { processName = proc.ProcessName; } catch (Exception ex) { }
+
+            // ProcessId can be accessed after the process has been killed but we'll do this safely anyways.
+            int pId = 0;
+            try { pId = proc.Id; } catch (Exception ex) { }
+
+            // Will only work if started by this instance of the dll.
+            try { proc.Kill(); } catch (Exception ex) { }
+
+            // Fallback to task kill
+            if (pId > 0)
+            {
+                var taskKilPsi = new ProcessStartInfo("taskkill");
+                taskKilPsi.Arguments = $"/pid {proc.Id} /T /F";
+                taskKilPsi.WindowStyle = ProcessWindowStyle.Hidden;
+                taskKilPsi.UseShellExecute = false;
+                taskKilPsi.RedirectStandardOutput = true;
+                taskKilPsi.RedirectStandardError = true;
+                taskKilPsi.CreateNoWindow = true;
+                var taskKillProc = Process.Start(taskKilPsi);
+                taskKillProc.WaitForExit();
+                String taskKillOutput = taskKillProc.StandardOutput.ReadToEnd(); // Contains success
+                String taskKillErrorOutput = taskKillProc.StandardError.ReadToEnd();
+            }
+
+            // Fallback to wmic delete process.
+            if (!string.IsNullOrEmpty(processName))
+            {
+                // https://stackoverflow.com/a/38757852/591285
+                var wmicPsi = new ProcessStartInfo("wmic");
+                wmicPsi.Arguments = $@"process where ""name='{processName}.exe'"" delete";
+                wmicPsi.WindowStyle = ProcessWindowStyle.Hidden;
+                wmicPsi.UseShellExecute = false;
+                wmicPsi.RedirectStandardOutput = true;
+                wmicPsi.RedirectStandardError = true;
+                wmicPsi.CreateNoWindow = true;
+                var wmicProc = Process.Start(wmicPsi);
+                wmicProc.WaitForExit();
+                String wmicOutput = wmicProc.StandardOutput.ReadToEnd(); // Contains success
+                String wmicErrorOutput = wmicProc.StandardError.ReadToEnd();
+            }
+
         }
 
         #endregion
